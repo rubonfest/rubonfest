c101151e3259b2ea6b3a5eafc9aa4d522ec05dec
Dzi <den.overtone@gmail.com>
2017-01-26 08:30:04 +0400

    Update content

--- Публиковать.cmd
+++ Публиковать.cmd
@@ -2,4 +2,4 @@
 
 git commit -m "Update content"
 
-git push origin master && lektor -O build build && lektor -O build deploy
+git push origin master && lektor --output-path build build && lektor --output-path build deploy
