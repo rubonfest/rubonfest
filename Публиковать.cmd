e75a4f727da457950ed05d854199c78a843701d3
Dzi <den.overtone@gmail.com>
2017-01-26 08:37:50 +0400

    Update content

--- Публиковать.cmd
+++ Публиковать.cmd
@@ -2,4 +2,4 @@
 
 git commit -m "Update content"
 
-git push origin master && lektor build -O build && lektor deploy -O build
+git push origin master && lektor build -O build && rsync -avz build/ repo@bs.miesta.by/var/www/rubon
