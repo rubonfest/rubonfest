885a798bccb7e6e98367894e7b590bbaebde8ccf
Dzianis Jackievič <den.overtone@gmail.com>
2017-02-20 07:54:31 +0300

    Big update

--- src/assets/static/js/main.js
+++ src/assets/static/js/main.js
@@ -1,4 +1,18 @@
 jQuery(function($) {
+
+  function openNav(e) {
+    document.getElementById("nav-overlay").style.width  = "100%";
+  }
+
+  function closeNav(e) {
+    document.getElementById("nav-overlay").style.width = "0%";
+    return false;
+  }
+
+  $('.nav-humb').click(openNav);
+  $('#nav-overlay .closebtn').click(closeNav);
+
   $('nav#nav-menu-sidebar').mmenu();
   $('html').niceScroll();
+  $('#news-content').niceScroll();
 });
