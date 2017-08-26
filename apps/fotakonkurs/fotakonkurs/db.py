27abf62a57c6b0cdf897c18c938b97ef2fc1f66f
Dzianis Jackievič <den.overtone@gmail.com>
2017-08-27 00:23:08 +0300

    length

--- apps/fotakonkurs/fotakonkurs/db.py
+++ apps/fotakonkurs/fotakonkurs/db.py
@@ -6,9 +6,9 @@ class UserFile(db.Model):
     __tablename__ = "user_file"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    filedir     = db.Column(db.Unicode)
-    filename    = db.Column(db.Unicode)
-    orig_name   = db.Column(db.Unicode)
+    filedir     = db.Column(db.Unicode(100))
+    filename    = db.Column(db.Unicode(100))
+    orig_name   = db.Column(db.Unicode(100))
 
     def __init__(self, orig_name, filename, filedir):
         self.orig_name = orig_name
