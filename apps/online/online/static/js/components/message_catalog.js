571aec1bb9c08527f0120904d5d0b1671198ca5b
Dzianis Jackievič <den.overtone@gmail.com>
2017-09-02 11:52:11 +0300

    added category links

--- apps/online/online/static/js/components/message_catalog.js
+++ apps/online/online/static/js/components/message_catalog.js
@@ -1,12 +1,15 @@
 define(['knockout'], function(ko) {
   function ViewModel(params) {
-    this.messageCategories = ko.computed(fetchCategories.bind(null, params.messagesData));
+    this.categoriesNames = ko.observableArray();
+    this.messageCategories = ko.computed(fetchCategories.bind(this, params.messagesData));
   }
   
   function fetchCategories(messagesData) {
     var data = messagesData();
     var result = [];
+    this.categoriesNames.removeAll();
     for (var cat in data) {
+      this.categoriesNames.push(cat);
       result.push(data[cat]);
     }
     return result;
